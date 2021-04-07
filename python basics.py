c2e4b89425b98b7ac1080e86d8516bbfc88f62ad
mercy435 <mercyisaac435@gmail.com>
2021-04-07 11:07:45 +0100

    update on files

--- python basics.py
+++ python basics.py
@@ -107,7 +107,7 @@ def test(a):
 
 
 help(test)
-print(test._doc_)
+print(test.doc)
 
 
 def super_func(*args):  # this allows more than one argument
@@ -126,7 +126,7 @@ def highest_even(list_):
     return max(evens)
 
 
-print(highest_even([10,2,3,4,5,11]))
+print(highest_even([10, 2, 3, 4, 5, 11]))
 
 
 def max_mercy(list_):
@@ -190,16 +190,18 @@ def checkDriverAge(age=0):
     if int(age) < 18:
         print("Sorry, you are too young to drive this car. Powering off")
     elif int(age) > 18:
-        print("Powering On. Enjoy the ride!");
+        print("Powering On. Enjoy the ride!")
     elif int(age) == 18:
         print("Congratulations on your first year of driving. Enjoy the ride!")
 
 
 checkDriverAge()
-# 1. Wrap the above code in a function called checkDriverAge(). Whenever you call this function, you will get prompted for age.
-# Notice the benefit in having checkDriverAge() instead of copying and pasting the function everytime?
+# 1. Wrap the above code in a function called checkDriverAge(). Whenever you call this function,
+# you will get prompted for age.
+# Notice the benefit in having checkDriverAge() instead of copying and pasting the function every time?
 
-# 2 Instead of using the input(). Now, make the checkDriverAge() function accept an argument of age, so that if you enter:
+# 2 Instead of using the input(). Now, make the checkDriverAge() function accept an argument of age,
+# so that if you enter:
 # checkDriverAge(92);
 # it returns "Powering On. Enjoy the ride!"
 # also make it so that the default age is set to 0 if no argument is given
